3524b2dc37b722875e850dc8e874d5eda13ec5d9
Juled Halili <juhalili@vsite.hr>
2024-11-09 15:40:46 +0100

    1. Klase i objekti

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,36 @@
 ﻿namespace ConsoleApp
 {
-    internal class Program
+    public class Proizvod
+    {
+
+        public string Naziv { get; set; }
+        public decimal Cijena { get; set; }
+        public int DostupnaKolicina { get; set; }
+
+
+        public Proizvod(string naziv, decimal cijena, int dostupnaKolicina)
+        {
+            Naziv = naziv;
+            Cijena = cijena;
+            DostupnaKolicina = dostupnaKolicina;
+        }
+
+        public void IspisiInformacije()
+        {
+            Console.WriteLine($"Naziv: {Naziv}");
+            Console.WriteLine($"Cijena: {Cijena} E");
+            Console.WriteLine($"Dostupna količina: {DostupnaKolicina}");
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Proizvod proizvod = new Proizvod("Auto", 40000, 10);
+
+            proizvod.IspisiInformacije();
         }
     }
 }
